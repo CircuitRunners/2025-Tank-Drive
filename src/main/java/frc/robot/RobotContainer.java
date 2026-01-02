d7e9258cf9e080e8d6ba636e396b8247b0e4ba7c
FryGuy <147878878+pranav1283@users.noreply.github.com>
2026-01-02 13:19:11 -0500

    Fixed the configureBindings method

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,6 +1,6 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.AimPivot;
@@ -12,7 +12,7 @@ import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Pivot;
 
 public class RobotContainer {
-    private final XboxController driverJoy = new XboxController(Constants.driverControllerPort);
+    private final CommandXboxController driverJoy = new CommandXboxController(Constants.driverControllerPort);
 
     private final Drive drive = new Drive();
     private final Pivot pivot = new Pivot();
